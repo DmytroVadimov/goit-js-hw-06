dfa361223d0a80f35df8241776167e628c693931
Dmytro Vadimov <104729263+DmytroVadimov@users.noreply.github.com>
2024-04-02 14:31:32 +0300

    task_2

--- js/task-2.js
+++ js/task-2.js
@@ -1,1 +1,24 @@
 'use strict';
+class Storage {
+  #items;
+  constructor(params) {
+    this.#items = params;
+  }
+  getItems() {
+    return this.#items;
+  }
+  addItem(newItem) {
+    this.#items.push(newItem);
+  }
+  removeItem(itemToRemove) {
+    const removeItems = this.#items.filter(item => item !== itemToRemove);
+    this.#items = removeItems;
+  }
+}
+
+const storage = new Storage(['Nanitoids', 'Prolonger', 'Antigravitator']);
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
+storage.addItem('Droid');
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
+storage.removeItem('Prolonger');
+console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
